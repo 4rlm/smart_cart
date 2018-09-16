3c393617d6c20b506446f26701a66f332b9371ce
Adam Booth <4rlm@protonmail.ch>
2018-09-16 08:50:28 -0500

    basic layout, schema, models, nav complete.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
+//= require jquery3
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require underscore
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
